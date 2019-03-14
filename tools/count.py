6800590620e76cb74394ef01a16bdb2b0af51d8f
Liu Linlin <liulinlin@mac.local>
2019-03-14 12:24:24 +0800

    update count

---
+++ tools/count.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import operator
+
+from nltk.tokenize import ToktokTokenizer
+
+filepath = './data/processed/test1/en-ne/out.5000000.en'
+
+
+def run(filepath):
+    toktok = ToktokTokenizer()
+    count = {}
+    with open(filepath, 'r') as inf:
+        for line in inf:
+            line = line.strip().lower()
+            for t in toktok.tokenize(line):
+                if t in count:
+                    count[t] += 1
+                else:
+                    count[t] = 0
+    outfile = filepath + '.count'
+    court_sort = sorted(count.items, key=operator.itemgetter(0), reverse=True)
+    with open(outfile, 'w') as outf:
+        for line in count_sort:
+            outf.write(' '.join(map(str, line)) + '\n')
+
+if __name__ == '__main__':
+    run(filepath)
