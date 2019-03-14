b08d6b040b8732fa9f0c6832fbc50b5ee2584f13
Liu Linlin <liulinlin@mac.local>
2019-03-14 12:27:40 +0800

    update

--- tools/count.py
+++ tools/count.py
@@ -19,7 +19,7 @@ def run(filepath):
                 else:
                     count[t] = 0
     outfile = filepath + '.count'
-    court_sort = sorted(count.items, key=operator.itemgetter(0), reverse=True)
+    court_sort = sorted(count.items(), key=operator.itemgetter(0), reverse=True)
     with open(outfile, 'w') as outf:
         for line in count_sort:
             outf.write(' '.join(map(str, line)) + '\n')
